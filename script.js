9f78672ee2270195b56274c0db2cb9f2230c342d
Asma Hassan <asmasheikhhassan@gmail.com>
2023-04-18 23:26:45 +0100

    Added button js

--- script.js
+++ script.js
@@ -2,3 +2,31 @@
 document.getElementById('location').style.display = 'none';
 document.getElementById('forecastD').style.display = 'none';
 
+//Button:
+
+var button = document.getElementById("search-button")
+var search = $("#search-input")
+
+button.addEventListener("click", function(event){
+    event.preventDefault();
+    var currentVal = search.val();
+
+    getWeatherData(currentVal).then(function(data){
+        displayWeatherData(data);
+    });
+        
+    getForecastData(currentVal).then(function(forecastData){
+        displayForecastData(forecastData);
+    })
+    
+    var cities = JSON.parse(localStorage.getItem("cityNames"));
+       
+      if (!cities){
+        cities = [];
+      }
+      if(!cities.find(city => city == currentVal)){
+        cities.push(currentVal); 
+      }
+
+    localStorage.setItem("cityNames", JSON.stringify(cities));
+});
